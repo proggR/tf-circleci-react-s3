2cfa1792f7b3b9439c8faf5a532a8e384c927886
Brando <sgt.groucho@gmail.com>
2022-06-23 22:28:07 -0400

    final test of app builds with npm audit fix --force for good measure (even though its just ancient create-react-app vulns that would never see prod atm

--- apps/clone-ci/src/App.js
+++ apps/clone-ci/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited <code>apps/clone-ci/src/App.js</code> and save to reload.
+          Edit <code>apps/clone-ci/src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
